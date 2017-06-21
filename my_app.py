980cab98f6f7949391833f4510207e1008b76ad2
Damien Robinson <damien.robinson@griffithuni.edu.au>
2017-06-21 14:37:19 +1000

    Added a route for the site map

--- my_app.py
+++ my_app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, send_from_directory
 from flask import render_template
 
 app = Flask(__name__)
@@ -6,6 +6,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/sitemap.xml')
+def static_from_root():
+    return send_from_directory(app.static_folder, request.path[1:])
 
 # We only need this for local development.
 if __name__ == '__main__':
