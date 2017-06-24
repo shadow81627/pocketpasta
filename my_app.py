e4b398eaa6f892d2cab844808f1720d89703fb4f
Damien Robinson <damien.robinson@griffithuni.edu.au>
2017-06-24 15:47:27 +1000

    Added custom 404 page

--- my_app.py
+++ my_app.py
@@ -11,9 +11,15 @@ def index():
 def catalogue():
     return render_template('catalogue.html')
 
+# Static sitemap
 @app.route('/sitemap.xml')
 def static_from_root():
     return send_from_directory(app.static_folder, request.path[1:])
+
+# Custom 404 page
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 # We only need this for local development.
 if __name__ == '__main__':
