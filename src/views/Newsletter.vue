f0edf6e703f20f4944f413f1780592cbde595161
Damien Robinson <shadow81627@gmail.com>
2018-09-24 22:11:11 +1000

    Format submit button html input

--- src/views/Newsletter.vue
+++ src/views/Newsletter.vue
@@ -24,13 +24,14 @@
         <div class="response" id="mce-error-response" style="display:none"></div>
         <div class="response" id="mce-success-response" style="display:none"></div>
       </div>
-      <!-- real people should not fill this in and expect good things 
+      <!-- real people should not fill this in and expect good things
           - do not remove this or risk form bot signups-->
       <div style="position: absolute; left: -5000px;" aria-hidden="true">
         <input type="text" name="b_7f69c361c7f5d97725d2e339b_c821016b08" tabindex="-1" value="">
       </div>
       <div class="clear">
-        <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
+        <input type="submit" value="Subscribe" name="subscribe"
+          id="mc-embedded-subscribe" class="button">
       </div>
     </div>
   </form>
