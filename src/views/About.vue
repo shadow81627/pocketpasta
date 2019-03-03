70ecb288454b7f1c6613e81ae7b8ee1bf29e1d72
Damien Robinson <damien.robinson@pocketpasta.com>
2019-03-03 20:55:18 +1000

    Add about text

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About Pocket Pasta</h1>
+      <p style="width: 80ch;margin: auto;">
+        Pocket Pasta is a progressive web app for journaling your life.
+        The aim is to have a multi module app that allows users to record how life works.
+        Documenting how we live is the first step in planning how to live.
+      </p>
   </div>
 </template>
