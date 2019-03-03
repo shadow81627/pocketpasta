7f0ab5b10af04a463e9da6bf7dc47614c0536e9d
Damien Robinson <damien.robinson@pocketpasta.com>
2019-03-03 20:55:02 +1000

    Update home page image and heading.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Pocket Pasta logo" src="../assets/logo.png">
+    <h1>Pocket Pasta</h1>
+    <!-- <HelloWorld msg="Pocket Pasta"/> -->
   </div>
 </template>
 
