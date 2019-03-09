702720683b948d6fb4258d554e5653a02c179a67
Damien Robinson <damien.robinson@pocketpasta.com>
2019-03-09 15:00:16 +1000

    Add Recipe component

---
+++ components/Recipe.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="recipe">
+    <h1>Recipe {{ $route.params.id }} Details</h1>
+    <p>I am recipe {{ recipe }}</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import linkData from '@/assets/link-data/spaghetti.json';
+export default {
+  computed: {
+    recipe() {
+      // parse id param to int for id lookup
+      const id = parseInt(this.$route.params.id, 10);
+      return this.$store.getters.getRecipeById(id);
+    },
+  },
+  head() {
+    return {
+      script: [
+        {
+          innerHTML: JSON.stringify(linkData),
+          type: 'application/ld+json',
+        },
+      ],
+      __dangerouslyDisableSanitizers: ['script'],
+    };
+  },
+};
+</script>
