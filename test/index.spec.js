7b4145ea394517b05b8d387755b755548d713603
Damien Robinson <damien.robinson@pocketpasta.com>
2019-03-09 14:54:42 +1000

    Add Jest for testing

---
+++ test/index.spec.js
@@ -0,0 +1,9 @@
+// const assert = require('assert');
+
+// describe('pocketpasta.com page', () => {
+//   it('should have the right title', () => {
+//     browser.url('https://pocketpasta.com');
+//     const title = browser.getTitle();
+//     assert.strict.equal(title, 'pocketpasta');
+//   });
+// });
