3debe109af87af9d9379347e57ba75264aaa0577
Damien Robinson <damien.robinson@pocketpasta.com>
2019-03-09 16:40:22 +1000

    Add recipe link data to vuex

--- store/index.js
+++ store/index.js
@@ -1,9 +1,10 @@
+import recipe from '@/assets/link-data/spaghetti.json';
+
 export const state = () => ({
-  recipe: {},
-  recipes: [1, 2, 3, 4],
+  recipes: [{ ...recipe, id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }],
 });
 
-export const getters = () => ({
+export const getters = {
   getRecipeById: (state) => (id) =>
-    state.recipes.find((recipe) => recipe === id),
-});
+    state.recipes.find((recipe) => recipe.id === id),
+};
