2c5ac4d1c0c45e29a3f82a56466288daf04349b6
Damien Robinson <damien.robinson@pocketpasta.com>
2019-03-10 08:12:04 +1000

    about page use social profile component

--- pages/about.vue
+++ pages/about.vue
@@ -1,31 +1,36 @@
 <template>
-  <div class="about">
-    <h1>About Pocket Pasta</h1>
-    <p style="width: 80ch;margin: auto;">
-      <!-- Pocket Pasta is a progressive web app for journaling your life.
-      The aim is to have a multi module app that allows users to record how they live.
-      Documenting how we live is the first step in planning how to live.-->
-      Pocket Pasta is a collection of my demo apps that I build to practice my
-      skills.
-    </p>
-    <h2>Pocket Pasta People</h2>
-    <ul style="list-style-type: none; padding: 0;">
-      <li>{{ damien.name }}</li>
-      <li>
-        <a :href="damien.email">{{ damien.email }}</a>
-      </li>
-      <li>{{ damien.jobTitle }}</li>
-      <li v-for="link in damien.sameAs" :key="link">
-        <a :href="link">{{ link }}</a>
-      </li>
-    </ul>
+  <div class="row">
+    <div class="col-lg-12 text-center">
+      <h1 class="mt-5">About Pocket Pasta</h1>
+      <p class="lead">
+        <!-- Pocket Pasta is a progressive web app for journaling your life.
+        The aim is to have a multi module app that allows users to record how they live.
+        Documenting how we live is the first step in planning how to live.-->
+        Pocket Pasta is a collection of my demo apps that I build to practice my
+        skills.
+      </p>
+      <!-- <ul class="list-unstyled">
+        <li>Bootstrap 4.2.1</li>
+        <li>jQuery 3.3.1</li>
+      </ul> -->
+    </div>
+    <div class="col-lg-12 text-center">
+      <!-- <h2 class="mt-5">Pocket Pasta People</h2> -->
+      <Profile v-bind="damien" />
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import damien from '@/assets/link-data/damien.json';
+
+import Profile from '@/components/ProfileSocial.vue';
+
 export default {
+  components: {
+    Profile,
+  },
   data: () => ({
     damien,
   }),
