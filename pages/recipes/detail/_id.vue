7f04926560419697af718b14869eb11a4f088d82
Damien Robinson <damien.robinson@pocketpasta.com>
2019-03-09 14:59:42 +1000

    Set recipe detail route param to be named id

--- pages/recipes/detail/_id.vue
+++ pages/recipes/detail/_id.vue
@@ -12,7 +12,7 @@ export default {
       // parse id param to int for id lookup
       const id = parseInt(this.$route.params.id, 10);
       return this.$store.getters.getRecipeById(id);
-    }
-  }
+    },
+  },
 };
 </script>
