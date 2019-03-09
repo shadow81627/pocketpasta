d6ddc4f49c1a55de4a9078e81bf573652c9335df
Damien Robinson <damien.robinson@pocketpasta.com>
2019-03-10 08:11:38 +1000

    Use boostrap 4 layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,31 +1,54 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <nuxt-link to="/" exact>Home</nuxt-link>|
-      <nuxt-link to="/about">About</nuxt-link>|
-      <nuxt-link to="/newsletter">Newsletter</nuxt-link>|
-      <nuxt-link to="/recipes" exact>Recipes</nuxt-link>
+    <!-- Navigation -->
+    <nav id="nav" class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <nuxt-link to="/" class="navbar-brand" exact>
+          PocketPasta
+        </nuxt-link>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarResponsive"
+          aria-controls="navbarResponsive"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon" />
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <nuxt-link to="/" tag="li" class="nav-item" exact>
+              <a class="nav-link">
+                Home<span class="sr-only">(current)</span>
+              </a>
+            </nuxt-link>
+            <nuxt-link tag="li" to="/about" class="nav-item">
+              <a class="nav-link">About</a>
+            </nuxt-link>
+            <nuxt-link tag="li" to="/newsletter" class="nav-item">
+              <a class="nav-link">Newsletter</a>
+            </nuxt-link>
+            <nuxt-link tag="li" to="/recipes" class="nav-item" exact>
+              <a class="nav-link">Recipes</a>
+            </nuxt-link>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+      <nuxt />
     </div>
-    <nuxt />
   </div>
 </template>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-a {
-  font-weight: bold;
-  color: #2c3e50;
+body {
+  /* font-family: 'Lato', 'sans-serif'; */
+  padding-top: 56px;
 }
 
 .nuxt-link-active {
