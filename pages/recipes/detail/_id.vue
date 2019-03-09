6bf1adc623c4f8d02e1d220f6b06f0371014fd94
Damien Robinson <damien.robinson@pocketpasta.com>
2019-03-10 08:12:32 +1000

    Add recipe details

--- pages/recipes/detail/_id.vue
+++ pages/recipes/detail/_id.vue
@@ -1,7 +1,21 @@
 <template>
   <div class="recipe">
-    <h1>Recipe {{ $route.params.id }} Details</h1>
-    <p>I am recipe {{ recipe.id }}</p>
+    <h1>{{ recipe.name }}</h1>
+    <p>{{ recipe.description }}</p>
+    <!-- <p>Author: {{ recipe.author }}</p> -->
+    <p>datePublished: {{ recipe.datePublished }}</p>
+    <p>Makes: {{ recipe.recipeYield }}</p>
+    <h2>recipeIngredient:</h2>
+    <ol>
+      <li
+        v-for="ingredient in recipe.recipeIngredient"
+        :key="ingredient"
+        style="list-style-position: inside;"
+      >
+        {{ ingredient }}
+      </li>
+    </ol>
+    <p>Tags: {{ recipe.keywords }}</p>
   </div>
 </template>
 
