d989578055b9602406b00631e20cc44a863cf2d7
Damien Robinson <damien.robinson@pocketpasta.com>
2019-03-10 16:30:01 +1000

    Add Keyvalues component

---
+++ components/KeyValue.vue
@@ -0,0 +1,19 @@
+<template>
+  <p>
+    <strong>{{ label }}:</strong>
+    <span>{{ value }}</span>
+  </p>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+    },
+    value: {
+      type: String,
+    },
+  },
+};
+</script>
