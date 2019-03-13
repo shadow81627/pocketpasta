c074cbaaa410321abd62b5192d1dab3e77ac2358
Damien Robinson <damien.robinson@pocketpasta.com>
2019-03-13 22:54:00 +1000

    remove white background on newsletter form

--- pages/newsletter.vue
+++ pages/newsletter.vue
@@ -72,7 +72,6 @@
 <style>
 @import url('//cdn-images.mailchimp.com/embedcode/classic-10_7.css');
 #mc_embed_signup {
-  background: #fff;
   clear: left;
   font: 14px Helvetica, Arial, sans-serif;
 }
