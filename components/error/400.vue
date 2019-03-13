cb31fd5de38a9599f4a318e8a27efb01ea2c05b7
Damien Robinson <damien.robinson@pocketpasta.com>
2019-03-13 22:52:14 +1000

    Add error components

---
+++ components/error/400.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <h1>Page not found</h1>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: () => {},
+    },
+  },
+};
+</script>
---
+++ components/error/500.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <h1>OOPSIE WOOPSIE!!</h1>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: () => {},
+    },
+  },
+};
+</script>
