4e5cf17cb63afcdfb6b99f6f3baf9bc76d2bf480
Damien Robinson <damien.robinson@pocketpasta.com>
2019-03-13 22:53:44 +1000

    Update 500 text

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,14 +1,31 @@
 <template>
-  <div class="container">
+  <div class="nuxt-error container">
+    <!-- <component :is="errorPage" :error="error" /> -->
     <h1 v-if="error.statusCode === 404">Page not found</h1>
-    <h1 v-else>An error occurred</h1>
+    <h1 v-else>OOPSIE WOOPSIE!!</h1>
     <nuxt-link to="/">Home page</nuxt-link>
   </div>
 </template>
 
 <script>
+// import error404 from '@/components/error/404.vue';
+// import error500 from '@/components/error/500.vue';
+
 export default {
-  props: ['error'],
-  layout: 'default', // you can set a custom layout for the error page
+  props: {
+    error: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  computed: {
+    // errorPage() {
+    //   if (this.statusCode === 404) {
+    //     return error404;
+    //   }
+    //   // catch everything else
+    //   return error500;
+    // },
+  },
 };
 </script>
