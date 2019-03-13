36c3e18c98a9d54f45e85a06a0633cfdb8259a15
Damien Robinson <damien.robinson@pocketpasta.com>
2019-03-14 06:51:35 +1000

    Add more themes

--- components/ThemeSelector.vue
+++ components/ThemeSelector.vue
@@ -65,6 +65,18 @@ export default {
     return {
       themes: [
         {
+          name: 'cerulean',
+          href: 'https://bootswatch.com/4/cerulean/bootstrap.min.css',
+        },
+        {
+          name: 'cosmo',
+          href: 'https://bootswatch.com/4/cosmo/bootstrap.min.css',
+        },
+        {
+          name: 'cyborg',
+          href: 'https://bootswatch.com/4/cyborg/bootstrap.min.css',
+        },
+        {
           value: 'darkly',
           href: 'https://bootswatch.com/4/darkly/bootstrap.min.css',
         },
@@ -73,12 +85,68 @@ export default {
           href: 'https://bootswatch.com/4/flatly/bootstrap.min.css',
         },
         {
+          value: 'journal',
+          href: 'https://bootswatch.com/4/journal/bootstrap.min.css',
+        },
+        {
+          value: 'litera',
+          href: 'https://bootswatch.com/4/litera/bootstrap.min.css',
+        },
+        {
+          value: 'lumen',
+          href: 'https://bootswatch.com/4/lumen/bootstrap.min.css',
+        },
+        {
+          value: 'lux',
+          href: 'https://bootswatch.com/4/lux/bootstrap.min.css',
+        },
+        {
           value: 'materia',
           href: 'https://bootswatch.com/4/materia/bootstrap.min.css',
         },
         {
+          value: 'minty',
+          href: 'https://bootswatch.com/4/minty/bootstrap.min.css',
+        },
+        {
+          value: 'pulse',
+          href: 'https://bootswatch.com/4/pulse/bootstrap.min.css',
+        },
+        {
+          value: 'sandstone',
+          href: 'https://bootswatch.com/4/sandstone/bootstrap.min.css',
+        },
+        {
+          value: 'simplex',
+          href: 'https://bootswatch.com/4/simplex/bootstrap.min.css',
+        },
+        {
+          value: 'sketchy',
+          href: 'https://bootswatch.com/4/sketchy/bootstrap.min.css',
+        },
+        {
+          value: 'slate',
+          href: 'https://bootswatch.com/4/slate/bootstrap.min.css',
+        },
+        {
           value: 'solar',
           href: 'https://bootswatch.com/4/solar/bootstrap.min.css',
+        },
+        {
+          value: 'spacelab',
+          href: 'https://bootswatch.com/4/spacelab/bootstrap.min.css',
+        },
+        {
+          value: 'superhero',
+          href: 'https://bootswatch.com/4/superhero/bootstrap.min.css',
+        },
+        {
+          value: 'untied',
+          href: 'https://bootswatch.com/4/untied/bootstrap.min.css',
+        },
+        {
+          value: 'yeti',
+          href: 'https://bootswatch.com/4/yeti/bootstrap.min.css',
         },
       ],
       themeHelper: new ThemeHelper(),
