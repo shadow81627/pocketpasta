8fa72c91ef31bcf2f1912e7ba07d825873e745fa
Damien Robinson <damien.robinson@pocketpasta.com>
2019-03-14 22:46:01 +1000

    Add desription to 500 page

--- components/error/500.vue
+++ components/error/500.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="container">
     <h1>OOPSIE WOOPSIE!!</h1>
+    <p>
+      Uwu We make a fucky wucky!! A wittle fucko boingo! The code monkeys at our
+      headquarters are working VEWY HAWD to fix this!
+    </p>
   </div>
 </template>
 
