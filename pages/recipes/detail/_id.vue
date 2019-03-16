d5c79f02b5de9f89d7b0189f1c7c100d20a8055f
Damien Robinson <damien.robinson@pocketpasta.com>
2019-03-16 19:17:00 +1000

    Remove unused function from recipe detail page

--- pages/recipes/detail/_id.vue
+++ pages/recipes/detail/_id.vue
@@ -9,12 +9,5 @@ export default {
   components: {
     Recipe,
   },
-  computed: {
-    recipe() {
-      // parse id param to int for id lookup
-      const id = parseInt(this.$route.params.id, 10);
-      return this.$store.getters.getRecipeById(id);
-    },
-  },
 };
 </script>
