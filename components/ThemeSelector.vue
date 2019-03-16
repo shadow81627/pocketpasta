6e964607f5bdc22a937458b10f78787f16381417
Damien Robinson <damien.robinson@pocketpasta.com>
2019-03-16 11:10:04 +1000

    Remove themehelper from theme selector component

--- components/ThemeSelector.vue
+++ components/ThemeSelector.vue
@@ -21,45 +21,6 @@
 </template>
 
 <script>
-const ThemeHelper = function() {
-  const preloadTheme = (href) => {
-    const link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.href = href;
-    document.head.appendChild(link);
-
-    return new Promise((resolve, reject) => {
-      link.onload = (e) => {
-        const sheet = e.target.sheet;
-        sheet.disabled = true;
-        resolve(sheet);
-      };
-      link.onerror = reject;
-    });
-  };
-
-  const selectTheme = (themes, name) => {
-    if (name && !themes[name]) {
-      throw new Error(`"${name}" has not been defined as a theme.`);
-    }
-    Object.keys(themes).forEach((n) => (themes[n].disabled = n !== name));
-  };
-
-  const themes = { darkly: true };
-
-  return {
-    add(name, href) {
-      return preloadTheme(href).then((s) => (themes[name] = s));
-    },
-    set theme(name) {
-      selectTheme(themes, name);
-    },
-    get theme() {
-      return Object.keys(themes).find((n) => !themes[n].disabled);
-    },
-  };
-};
-
 export default {
   data() {
     return {
@@ -149,7 +110,6 @@ export default {
           href: 'https://bootswatch.com/4/yeti/bootstrap.min.css',
         },
       ],
-      themeHelper: new ThemeHelper(),
     };
   },
   computed: {
