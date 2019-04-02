2967732f228b4f33f7eb7a2f2f6789d3781e41bc
Damien Robinson <damien.robinson@pocketpasta.com>
2019-04-02 21:30:19 +1000

    Add missing recipe details page

---
+++ pages/recipes/detail/_id/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <Recipe />
+</template>
+
+<script>
+import Recipe from '@/components/Recipe.vue';
+
+export default {
+  components: {
+    Recipe,
+  },
+  data() {
+    return { id: this.$route.params.id };
+  },
+  head() {
+    return {
+      link: [
+        {
+          rel: 'canonical',
+          href: `https://pocketpasta.com/recipes/${this.id}`,
+        },
+      ],
+    };
+  },
+};
+</script>
