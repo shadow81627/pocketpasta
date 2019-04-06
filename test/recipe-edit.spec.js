2e4bf48f259b93e49a3364c055665d24152e1543
Damien Robinson <damien.robinson@pocketpasta.com>
2019-04-06 10:30:49 +1000

    Add test for recipe edit page

---
+++ test/recipe-edit.spec.js
@@ -0,0 +1,67 @@
+import { shallowMount, createLocalVue, RouterLinkStub } from '@vue/test-utils';
+import component from '@/pages/recipes/detail/_id/edit.vue';
+import Vuex from 'vuex';
+import VueRouter from 'vue-router';
+
+import spaghetti from '@/assets/link-data/spaghetti.json';
+
+const localVue = createLocalVue();
+
+localVue.use(Vuex);
+
+localVue.use(VueRouter);
+
+const router = new VueRouter();
+
+let getters;
+let state;
+let store;
+let mutations;
+
+beforeEach(() => {
+  state = {
+    recipes: [{ ...spaghetti, id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }],
+  };
+
+  getters = {
+    getRecipeById: (state) => (id) =>
+      state.recipes.find((recipe) => recipe.id === id),
+  };
+
+  mutations = {
+    setTheme(currentState, theme) {
+      currentState.theme = theme;
+    },
+  };
+
+  store = new Vuex.Store({
+    state,
+    getters,
+    mutations,
+  });
+});
+
+const factory = () =>
+  shallowMount(component, {
+    store,
+    router,
+    stubs: {
+      NuxtLink: RouterLinkStub,
+    },
+    localVue,
+  });
+
+describe('recipe-edit', () => {
+  test('mounts properly', () => {
+    const wrapper = factory();
+    expect(wrapper.isVueInstance()).toBeTruthy();
+  });
+
+  test('renders properly', () => {
+    const wrapper = factory();
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+  test('head', () => {
+    expect(component.head()).toBeTruthy();
+  });
+});
