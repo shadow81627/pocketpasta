285a111b3720c67c70d1364fe801e0f0402bedf7
Damien Robinson <damien.robinson@pocketpasta.com>
2019-04-13 08:38:23 +1000

    Add fetchUser to login page

--- pages/login.vue
+++ pages/login.vue
@@ -4,3 +4,11 @@
     <code>{{ JSON.stringify($auth.user, null, 2) }}</code>
   </div>
 </template>
+
+<script>
+export default {
+  async fetch(context) {
+    await context.app.$auth.fetchUser();
+  },
+};
+</script>
