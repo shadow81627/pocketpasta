880d2123d9a6e47736b59ae5724e40db8e36cc45
Damien Robinson <damien.robinson@pocketpasta.com>
2019-04-14 20:51:15 +1000

    Change theme middlewrare to sync vuex

--- middleware/theme.js
+++ middleware/theme.js
@@ -1,4 +1,8 @@
 export default function(context) {
-  const theme = context.app.$auth.$storage.getUniversal('theme', true);
+  const theme = context.app.$auth.$storage.syncUniversal(
+    'theme',
+    context.store.state.theme,
+    true,
+  );
   context.store.commit('setTheme', theme);
 }
