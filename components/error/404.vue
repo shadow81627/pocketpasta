2616412cd04f8e38c9a7b206ca6bfc667b9df08d
Damien Robinson <damien.robinson@pocketpasta.com>
2019-04-19 08:40:44 +1000

    Add  bootstrap styling to 404

--- components/error/404.vue
+++ components/error/404.vue
@@ -1,12 +1,12 @@
 <template>
-  <div id="message">
+  <div id="message" class="card bg-light card-body">
     <h2>404</h2>
     <h1>Page Not Found</h1>
     <p>
       The specified file was not found on this website. Please check the URL for
       mistakes and try again.
     </p>
-    <nuxt-link to="/">Home page</nuxt-link>
+    <nuxt-link to="/" class="btn btn-primary">Home page</nuxt-link>
   </div>
 </template>
 
@@ -27,28 +27,20 @@ export default {
 </script>
 
 <style media="screen" scoped>
-body {
-  background: #eceff1;
-  color: rgba(0, 0, 0, 0.87);
-  font-family: Roboto, Helvetica, Arial, sans-serif;
-  margin: 0;
-  padding: 0;
-}
 #message {
-  background: white;
   max-width: 360px;
   margin: 100px auto 16px;
   padding: 32px 24px 16px;
   border-radius: 3px;
 }
 #message h3 {
-  color: #888;
+  /* color: #888; */
   font-weight: normal;
   font-size: 16px;
   margin: 16px 0 12px;
 }
 #message h2 {
-  color: #4dba87;
+  /* color: #4dba87; */
   font-weight: bold;
   font-size: 16px;
   margin: 0 0 8px;
@@ -56,7 +48,7 @@ body {
 #message h1 {
   font-size: 22px;
   font-weight: 300;
-  color: rgba(0, 0, 0, 0.6);
+  /* color: rgba(0, 0, 0, 0.6); */
   margin: 0 0 16px;
 }
 #message p {
@@ -67,10 +59,10 @@ body {
 #message a {
   display: block;
   text-align: center;
-  background: #039be5;
+  /* background: #039be5; */
   text-transform: uppercase;
   text-decoration: none;
-  color: white;
+  /* color: white; */
   padding: 16px;
   border-radius: 4px;
 }
@@ -82,16 +74,5 @@ body {
   color: rgba(0, 0, 0, 0.4);
   text-align: center;
   font-size: 13px;
-}
-@media (max-width: 600px) {
-  body,
-  #message {
-    margin-top: 0;
-    background: white;
-    box-shadow: none;
-  }
-  body {
-    border-top: 16px solid #4dba87;
-  }
 }
 </style>
