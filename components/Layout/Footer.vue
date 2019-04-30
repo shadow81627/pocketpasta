28f6453431cdd745c8f5e823eef7143e86109a9d
Damien Robinson <damien.robinson@pocketpasta.com>
2019-04-30 22:46:18 +1000

    Hide footer in print

--- components/Layout/Footer.vue
+++ components/Layout/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-footer class="pa-3" inset app absolute>
+  <v-footer class="pa-3 hidden-print-only" inset app absolute>
     <v-spacer />
     <small class="text-muted"
       >This site is protected by reCAPTCHA and the Google
