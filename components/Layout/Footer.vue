2aa3a793d542a8b51f9f437221658e0f73a2a94e
Damien Robinson <damien.robinson@pocketpasta.com>
2019-05-05 01:52:07 +1000

    fix(footer): set height auto to fit content

--- components/Layout/Footer.vue
+++ components/Layout/Footer.vue
@@ -1,12 +1,16 @@
 <template>
-  <v-footer class="pa-3 hidden-print-only" xinset app absolute>
-    <small class="text-muted">version: {{ version }}</small>
+  <v-footer class="pa-3 hidden-print-only" xinset app absolute height="auto">
+    <v-card flat color="transparent">
+      <v-card-text class="text-muted">version: {{ version }}</v-card-text>
+    </v-card>
     <v-spacer />
-    <small class="text-muted"
-      >This site is protected by reCAPTCHA and the Google
-      <a href="https://policies.google.com/privacy">Privacy Policy</a> and
-      <a href="https://policies.google.com/terms">Terms of Service</a> apply.
-    </small>
+    <v-card flat color="transparent">
+      <v-card-text class="text-muted"
+        >This site is protected by reCAPTCHA and the Google
+        <a href="https://policies.google.com/privacy">Privacy Policy</a> and
+        <a href="https://policies.google.com/terms">Terms of Service</a> apply.
+      </v-card-text>
+    </v-card>
   </v-footer>
 </template>
 
