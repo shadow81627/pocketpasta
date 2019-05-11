472e0ffd9fc6ed4d6e8de8c827e33c8fb69fed2e
Damien Robinson <damien.robinson@pocketpasta.com>
2019-05-12 08:45:30 +1000

    refactor(recipes): use LIst

--- pages/recipes/index.vue
+++ pages/recipes/index.vue
@@ -1,48 +1,18 @@
 <template>
   <div class="container">
-    <div class="recipes">
-      <h1>{{ $t('recipes.heading') }}</h1>
-      <div class="row justify-content-end">
-        <ViewSwitcher v-model="layout" />
-      </div>
-      <div class="row">
-        <div class="col-12">
-          <div class="card-layout" :class="layoutClass">
-            <RecipeCard
-              v-for="recipe in recipes"
-              :key="recipe.id"
-              v-bind="recipe"
-              :layout="layout"
-            />
-          </div>
-        </div>
-      </div>
-    </div>
+    <List v-bind="{ heading: $t('recipes.heading'), list, layout: 'list' }" />
   </div>
 </template>
 
 <script>
-import ViewSwitcher from '@/components/List/ViewSwitcher';
-import RecipeCard from '@/components/List/Card';
+import List from '@/components/List/List';
 export default {
   components: {
-    ViewSwitcher,
-    RecipeCard,
+    List,
   },
-  data: () => ({
-    layout: null,
-  }),
   computed: {
-    recipes() {
+    list() {
       return this.$store.state.recipes;
-    },
-    layoutClass() {
-      // list layout is no class
-      return {
-        'card-group': this.layout === 'group',
-        'card-deck': this.layout === 'deck',
-        'card-columns': this.layout === 'columns',
-      };
     },
   },
   head() {
