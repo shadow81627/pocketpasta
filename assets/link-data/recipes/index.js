b13d255038dcdb94acd63b44c2619ff73e6d71bc
Damien Robinson <damien.robinson@pocketpasta.com>
2019-07-19 19:14:30 +1000

    chore(recipe): add recipe 5

--- assets/link-data/recipes/index.js
+++ assets/link-data/recipes/index.js
@@ -2,5 +2,6 @@ import recipe from './spaghetti.json';
 import pastaAglioEOlio from './pasta_aglio_e_olio.json';
 import lasagnaSalad from './lasagna-salad.json';
 import roastTaters from './roast-taters.json';
+import recipe5 from './vegan_hot_and_sour_soup_with_bok_choy.json';
 
-export default [recipe, pastaAglioEOlio, lasagnaSalad, roastTaters];
+export default [recipe, pastaAglioEOlio, lasagnaSalad, roastTaters, recipe5];
