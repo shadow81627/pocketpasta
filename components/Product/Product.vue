819cee79d8c3f93ee80e1f5d6bed9dbd5c7b93d7
Damien Robinson <damien.robinson@pocketpasta.com>
2019-08-14 17:29:26 +1000

    feat(product): add offers

--- components/Product/Product.vue
+++ components/Product/Product.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="product">
-    <h1>{{ product.name }}</h1>
+  <div class="product" itemscope itemtype="http://schema.org/Product">
+    <h1 itemprop="name">{{ product.name }}</h1>
     <p>{{ product.description }}</p>
 
     <b-img-lazy
@@ -8,24 +8,56 @@
       :src="product.image"
       class="img-fluid mx-auto d-block"
       :alt="product.name"
+      itemprop="image"
     />
 
     <share class="py-4" />
 
-    <p>
+    <p v-if="product.recipeYield">
       <strong>Servings:</strong>
       <span>{{ product.recipeYield }}</span>
     </p>
-    <h2>Ingredients:</h2>
-    <ol class="list-group-flush">
-      <li
-        v-for="ingredient in product.ingredient"
-        :key="ingredient"
-        class="list-group-item"
+    <div v-if="product.ingredient">
+      <h2>Ingredients:</h2>
+      <ol class="list-group-flush">
+        <li
+          v-for="ingredient in product.ingredient"
+          :key="ingredient"
+          class="list-group-item"
+        >
+          {{ ingredient }}
+        </li>
+      </ol>
+    </div>
+
+    <div
+      v-if="product.offers.offers"
+      itemprop="offers"
+      itemscope
+      itemtype="http://schema.org/AggregateOffer"
+    >
+      <h2>Sellers</h2>
+      <div
+        v-for="(offer, index) in product.offers.offers"
+        :key="index"
+        itemprop="offers"
+        itemscope
+        itemtype="http://schema.org/Offer"
+        class="list-group-flush"
       >
-        {{ ingredient }}
-      </li>
-    </ol>
+        <span class="list-group-item">
+          <a :href="offer.url" target="_blank" itemprop="url">
+            <span itemprop="offeredBy">{{ offer.offeredBy }}</span></a
+          >
+          <span itemprop="price">{{
+            new Intl.NumberFormat('en-AU', {
+              style: 'currency',
+              currency: offer.priceCurrency || 'AUD',
+            }).format(offer.price)
+          }}</span></span
+        >
+      </div>
+    </div>
 
     <nutrition-fact-table
       v-if="
