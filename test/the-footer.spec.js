d3c0561783608cb89e4e2208b431172d971d4569
Damien Robinson <damien.robinson@pocketpasta.com>
2019-08-31 18:11:07 +1000

    test(footer): set data to make the snapshot the same across builds

--- test/the-footer.spec.js
+++ test/the-footer.spec.js
@@ -9,7 +9,14 @@ localVue.use(Vuex);
 localVue.use(Vuetify);
 
 describe('Footer', () => {
-  const factory = () => shallowMount(Component, { localVue });
+  const factory = () =>
+    shallowMount(Component, {
+      localVue,
+      data: () => ({
+        version: '0.11.19',
+        commit: 'a95a529a01ab4fe627cc276b35ed8f5e0c6f45d9',
+      }),
+    });
 
   test('mounts properly', () => {
     const wrapper = factory();
