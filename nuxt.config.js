787266f4b873c5fbf6508f8a464f38df49ad0775
Damien Robinson <damien.robinson@pocketpasta.com>
2019-09-08 23:45:51 +1000

    fix(viewport): add shrink-to-fit=no bootstrap requires this for safari

--- nuxt.config.js
+++ nuxt.config.js
@@ -84,7 +84,8 @@ module.exports = {
       },
       {
         name: 'viewport',
-        content: 'width=device-width, initial-scale=1, minimal-ui',
+        content:
+          'width=device-width, initial-scale=1, shrink-to-fit=no, minimal-ui',
       },
       {
         hid: 'description',
