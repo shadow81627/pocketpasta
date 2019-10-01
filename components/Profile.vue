00e5b31f26ae61b55d5eebffec38319b3b435bc5
Damien Robinson <damien.robinson@pocketpasta.com>
2019-10-01 22:40:53 +1000

    fix(profile): rename v-list-tile to v-list-item

--- components/Profile.vue
+++ components/Profile.vue
@@ -1,17 +1,18 @@
 <template>
-  <v-list>
-    <v-list-tile avatar>
-      <v-list-tile-avatar tile>
+  <v-list v-if="$auth.loggedIn">
+    <v-list-item avatar>
+      <v-list-item-avatar>
         <b-img-lazy :src="$auth.user.picture" alt="avatar" class="rounded" />
-      </v-list-tile-avatar>
+      </v-list-item-avatar>
 
-      <v-list-tile-content>
-        <v-list-tile-title v-if="$auth.user.name !== $auth.user.email">{{
+      <v-list-item-content>
+        <v-list-item-title v-if="$auth.user.name !== $auth.user.email">{{
           $auth.user.name
-        }}</v-list-tile-title>
-        <v-list-tile-title>{{ $auth.user.email }}</v-list-tile-title>
-      </v-list-tile-content>
-    </v-list-tile>
+        }}</v-list-item-title>
+        <v-list-item-title>{{ $auth.user.email }}</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+  </v-list>
 
   <v-list v-else-if="$store.state.user">
     <v-list-item avatar>
