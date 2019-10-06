9351143972bd4d917e22d8a8a2bce74bc566e58e
Damien Robinson <damien.robinson@pocketpasta.com>
2019-10-06 21:30:24 +1000

    chore(firebase): add firestore

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,5 +1,5 @@
 import firebase from 'firebase/app';
-// import 'firebase/firestore';
+import 'firebase/firestore';
 import 'firebase/performance';
 import 'firebase/auth';
 import 'firebase/analytics';
@@ -43,12 +43,34 @@ export default async ({ $axios, isDev }, inject) => {
       firebase.initializeApp(config);
     }
   }
-  // const firestore = firebase.firestore();
+  const firestore = firebase.firestore();
   // Initialize Performance Monitoring and get a reference to the service
   const performance = firebase.performance();
   const auth = firebase.auth;
   const analytics = firebase.analytics();
 
-  await inject('firebase', { performance, analytics, auth });
-  return { performance, analytics, auth };
+  // The default cache size threshold is 40 MB. Configure "cacheSizeBytes"
+  // for a different threshold (minimum 1 MB) or set to "CACHE_SIZE_UNLIMITED"
+  // to disable clean-up.
+  firebase.firestore().settings({
+    cacheSizeBytes: firebase.firestore.CACHE_SIZE_UNLIMITED,
+  });
+
+  firebase
+    .firestore()
+    .enablePersistence()
+    .catch(function(err) {
+      if (err.code === 'failed-precondition') {
+        // Multiple tabs open, persistence can only be enabled
+        // in one tab at a a time.
+      } else if (err.code === 'unimplemented') {
+        // The current browser does not support all of the
+        // features required to enable persistence
+      }
+    });
+
+  const firebaseModules = { performance, analytics, auth, firestore };
+
+  await inject('firebase', firebaseModules);
+  return firebaseModules;
 };
