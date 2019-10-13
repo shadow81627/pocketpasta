25e4981324a85e57cfc6f2c9289ee826391843d5
Damien Robinson <damien.robinson@pocketpasta.com>
2019-10-13 23:02:55 +1000

    fix(changelog): make title a heading 1

--- release.config.js
+++ release.config.js
@@ -27,7 +27,7 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    ['@semantic-release/changelog', { changelogTitle: 'Releases' }],
+    ['@semantic-release/changelog', { changelogTitle: '# Releases' }],
     '@semantic-release/npm',
     [
       '@semantic-release/git',
