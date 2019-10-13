a497b109b6d4e556201117136e0aef652abaa0ab
Damien Robinson <damien.robinson@pocketpasta.com>
2019-10-13 20:32:56 +1000

    feat(about): add pitch video

--- pages/about.vue
+++ pages/about.vue
@@ -60,6 +60,23 @@
       </two-by-one>
       <two-by-one />
 
+      <div class="row">
+        <div class="col">
+          <h2>The Pitch</h2>
+          <div class="embed-responsive embed-responsive-16by9 d-print-none">
+            <iframe
+              width="560"
+              height="315"
+              class="embed-responsive-item"
+              src="https://www.youtube.com/embed/y2Z_FANVIuk"
+              frameborder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            />
+          </div>
+        </div>
+      </div>
+
       <!-- <div class="row">
         <div class="card bg-light card-body mb-3 profile">
           <div class="embed-responsive embed-responsive-16by9">
