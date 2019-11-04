796947de991d96cece3723fef5fe58437a4d6e7e
Damien Robinson <damien.robinson@pocketpasta.com>
2019-11-04 22:27:59 +1000

    feat(shoppinglsit): add more toolbar options

--- pages/shoppinglist.vue
+++ pages/shoppinglist.vue
@@ -138,9 +138,23 @@ export default {
     const editor = new Quill('#editor', {
       modules: {
         toolbar: [
-          ['bold', 'italic'],
+          ['bold', 'italic', 'underline', 'strike'], // toggled buttons
           ['link', 'blockquote', 'image'],
+
+          [{ header: 1 }, { header: 2 }], // custom button values
           [{ list: 'ordered' }, { list: 'bullet' }, { list: 'check' }],
+          [{ script: 'sub' }, { script: 'super' }], // superscript/subscript
+          [{ indent: '-1' }, { indent: '+1' }], // outdent/indent
+          [{ direction: 'rtl' }], // text direction
+
+          [{ size: ['small', false, 'large', 'huge'] }], // custom dropdown
+          // [{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+          [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+          [{ font: [] }],
+          [{ align: [] }],
+
+          ['clean'],
         ],
       },
       theme: 'snow',
