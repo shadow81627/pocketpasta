da7c1e54aaaaf195834340ffbddfceb639cefa12
Damien Robinson <damien.robinson@pocketpasta.com>
2019-12-28 17:16:54 +1000

    feat(fraction-text): bold numbers

--- components/FractionText.vue
+++ components/FractionText.vue
@@ -1,5 +1,10 @@
 <template>
-  <span v-if="Array.isArray(text.split(fractionsRegex))">
+  <span
+    v-if="
+      Array.isArray(text.split(fractionsRegex)) &&
+        text.split(fractionsRegex).length > 1
+    "
+  >
     <span
       v-for="part in text.split(fractionsRegex)"
       :key="part"
@@ -7,6 +12,16 @@
         frac: part.search(fractionsRegex) !== -1,
         'font-weight-bold':
           part.search(/^\d+$/) !== -1 || part.search(fractionsRegex) !== -1,
+      }"
+      >{{ part }}</span
+    ></span
+  >
+  <span v-else-if="Array.isArray(text.split(numberRegex))">
+    <span
+      v-for="part in text.split(numberRegex)"
+      :key="part"
+      :class="{
+        'font-weight-bold': part.search(numberRegex) !== -1,
       }"
       >{{ part }}</span
     ></span
@@ -24,6 +39,7 @@ export default {
   },
   data() {
     return {
+      numberRegex: /(\d+)/g,
       fractionsRegex: /([1-9][0-9]*\/[1-9][0-9]*)/g,
     };
   },
