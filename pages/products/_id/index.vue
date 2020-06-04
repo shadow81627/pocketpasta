20d83b7e932fd2bae3f3636a0526106d4c7311bf
Damien Robinson <damien.robinson@pocketpasta.com>
2020-06-04 16:51:51 +1000

    refactor(products): use content module

--- pages/products/_id/index.vue
+++ pages/products/_id/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <product />
+    <product v-bind="item" />
   </div>
 </template>
 
@@ -10,6 +10,11 @@ import Product from '@/components/Product/Product';
 export default {
   components: {
     Product,
+  },
+  async asyncData(context) {
+    const id = parseInt(context.route.params.id, 10);
+    const item = await context.$content('products', id).fetch();
+    return { id, item };
   },
   data() {
     return { id: this.$route.params.id };
@@ -24,6 +29,10 @@ export default {
         },
       ],
     };
+  },
+  validate({ params }) {
+    // Must be a number
+    return /^\d+$/.test(params.id);
   },
 };
 </script>
