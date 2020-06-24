e1271b9bc9b1add41b0e0508e0f35a7bf12f962b
Damien Robinson <damien.robinson@pocketpasta.com>
2020-06-24 10:49:17 +1000

    test: add lastModified and search

---
+++ test/components/lastModified.spec.js
@@ -0,0 +1,35 @@
+import Component from '@/components/Layout/lastModified.vue';
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import Vuex from 'vuex';
+import Vuetify from 'vuetify';
+import Vue from 'vue';
+
+const localVue = createLocalVue();
+let vuetify;
+
+localVue.use(Vuex);
+Vue.use(Vuetify);
+
+const factory = () =>
+  shallowMount(Component, {
+    localVue,
+    vuetify,
+    propsData: {
+      utc: true,
+    },
+  });
+
+describe('Last Modified', () => {
+  beforeEach(() => {
+    vuetify = new Vuetify();
+  });
+  test('mounts properly', () => {
+    const wrapper = factory();
+    expect(wrapper.vm).toBeTruthy();
+  });
+
+  test('renders properly', () => {
+    const wrapper = factory();
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+});
