44bd72f8e4bf5dc7726f8ba66f77594305416878
Damien Robinson <damien.robinson@pocketpasta.com>
2020-07-01 09:02:50 +1000

    feat(scrape): read write files (#1242)
    * feat(scrape): read write files
    
    * fix(scrape): add blacklist
    
    * test: add offers
    
    * refactor(scrape): move instruction text replace

---
+++ test/pages/organizations.spec.js
@@ -0,0 +1,44 @@
+import Component from '@/pages/organizations.vue';
+import { shallowMount, createLocalVue, RouterLinkStub } from '@vue/test-utils';
+import Vuex from 'vuex';
+import VueRouter from 'vue-router';
+import Vuetify from 'vuetify';
+import BootstrapVuePlugin from 'bootstrap-vue';
+import Vue from 'vue';
+
+const localVue = createLocalVue();
+
+localVue.use(Vuex);
+localVue.use(VueRouter);
+Vue.use(Vuetify);
+localVue.use(BootstrapVuePlugin);
+
+const router = new VueRouter();
+
+let vuetify;
+
+beforeEach(() => {
+  vuetify = new Vuetify();
+});
+
+const factory = () =>
+  shallowMount(Component, {
+    router,
+    vuetify,
+    stubs: {
+      NuxtLink: RouterLinkStub,
+    },
+    localVue,
+  });
+
+describe('organizations page', () => {
+  test('mounts properly', () => {
+    const wrapper = factory();
+    expect(wrapper.vm).toBeTruthy();
+  });
+
+  test('renders properly', () => {
+    const wrapper = factory();
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+});
