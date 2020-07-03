5834b69de1046296c8e1483b4c2cb54755b2d306
Damien Robinson <damien.robinson@pocketpasta.com>
2020-07-03 13:22:28 +1000

    fix(unsplash-logo): add style sheet (#1286)
    Prevent stylesheet being stripped.
    
    resolves #1285

--- components/Layout/Credit/landing-hero-image-credit.vue
+++ components/Layout/Credit/landing-hero-image-credit.vue
@@ -18,19 +18,10 @@
     target="_blank"
     rel="noopener noreferrer"
     title="Download free do whatever you want high-resolution photos from Icons8 team"
-    ><span style="display: inline-block; padding: 2px 3px;"
-      ><svg
-        xmlns="http://www.w3.org/2000/svg"
-        style="
-          height: 12px;
-          width: auto;
-          position: relative;
-          vertical-align: middle;
-          top: -2px;
-          fill: white;
-        "
-        viewBox="0 0 32 32"
-      >
+    ><span
+      style="display: inline-block; padding: 2px 3px;"
+      class="unsplash-logo"
+      ><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
         <title>unsplash-logo</title>
         <path d="M10 9V0h12v9H10zm12 5h10v18H0V14h10v9h12v-9z" /></svg></span
     ><span style="display: inline-block; padding: 2px 3px;"
@@ -38,3 +29,14 @@
     ></a
   >
 </template>
+
+<style scoped>
+.unsplash-logo svg {
+  height: 12px;
+  width: auto;
+  position: relative;
+  vertical-align: middle;
+  top: -2px;
+  fill: white;
+}
+</style>
