eedd3ea2a81234650689808b91d28ec082ab4009
Damien Robinson <damien.robinson@pocketpasta.com>
2020-07-07 21:25:57 +1000

    refactor(add-to-home-screen): copy from vue-pwa-install

--- test/components/AddToHomeScreen.spec.js
+++ test/components/AddToHomeScreen.spec.js
@@ -1,5 +1,5 @@
 import component from '@/components/AddToHomeScreen.vue';
-import { shallowMount, createLocalVue } from '@vue/test-utils';
+import { mount, createLocalVue } from '@vue/test-utils';
 import Vuetify from 'vuetify';
 import BootstrapVuePlugin from 'bootstrap-vue';
 import Vue from 'vue';
@@ -10,12 +10,38 @@ let vuetify;
 Vue.use(Vuetify);
 localVue.use(BootstrapVuePlugin);
 
+const eventMock = { prompt() {}, preventDefault() {} };
+const eventMap = {};
+
+window.addEventListener = jest.fn((event, cb) => {
+  if (event in eventMap) {
+    eventMap[event].push(cb);
+  } else {
+    eventMap[event] = [cb];
+  }
+});
+
+function triggerWindowEvent(event, payload) {
+  if (event in eventMap) {
+    eventMap[event].forEach((callback) => callback(payload));
+  }
+}
+
 describe('AddToHomeScreen', () => {
   beforeEach(() => {
     vuetify = new Vuetify();
   });
 
-  const factory = () => shallowMount(component, { localVue, vuetify });
+  const factory = () =>
+    mount(component, {
+      localVue,
+      vuetify,
+      mocks: {
+        process: {
+          browser: true,
+        },
+      },
+    });
 
   test('mounts properly', () => {
     const wrapper = factory();
@@ -27,9 +53,17 @@ describe('AddToHomeScreen', () => {
     expect(wrapper.html()).toMatchSnapshot();
   });
 
-  test('events', () => {
+  test("emits 'canInstall' on 'beforeinstallprompt' event", async () => {
     const wrapper = factory();
-    wrapper.trigger('beforeinstallprompt');
-    wrapper.trigger('click');
+    expect(wrapper.vm).toBeTruthy();
+    expect(wrapper.vm.deferredPrompt).toBe(null);
+    triggerWindowEvent('beforeinstallprompt', eventMock);
+    await wrapper.vm.$nextTick();
+    expect(wrapper.emitted('canInstall')).toBeTruthy();
+    expect(wrapper.emitted('canInstall').length).toBe(1);
+    expect(wrapper.emitted('canInstall')[0]).toStrictEqual([eventMock]);
+    await wrapper.vm.$nextTick();
+    await wrapper.trigger('click');
+    expect(wrapper.vm.deferredPrompt).toBe(eventMock);
   });
 });
