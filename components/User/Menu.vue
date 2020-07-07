8f668e0d682bb352d9ea01d90a91fda5b9d5c61d
Damien Robinson <damien.robinson@pocketpasta.com>
2020-07-07 16:14:47 +1000

    fix(user-menu): add placeholder

--- components/User/Menu.vue
+++ components/User/Menu.vue
@@ -1,65 +1,84 @@
 <template>
-  <client-only>
-    <v-menu v-model="menu" :close-on-content-click="false" offset-y>
-      <template v-slot:activator="{ on }">
-        <v-btn v-if="$auth.loggedIn" icon large aria-label="avatar" v-on="on">
-          <v-avatar size="32px" tile>
-            <v-img
-              v-if="$auth.user.picture"
-              :src="$auth.user.picture"
-              width="32"
-              height="32"
-              class="rounded"
-              alt="avatar"
-              onerror="javascript:this.style.display = 'none'"
-              throttle="100"
-            />
-          </v-avatar>
-        </v-btn>
-        <v-btn
-          v-else
-          aria-label="login"
-          outlined
-          @click="$auth.loginWith('custom')"
-        >
-          <v-icon>$account_circle</v-icon>
-          <span>&nbsp;</span>
-          <span>{{ $t('layout.login') }}</span>
-        </v-btn>
-      </template>
+  <div>
+    <client-only>
+      <v-menu v-model="menu" :close-on-content-click="false" offset-y>
+        <template v-slot:activator="{ on }">
+          <v-btn
+            v-if="$auth.loggedIn"
+            icon
+            large
+            aria-label="avatar"
+            min-width="32"
+            v-on="on"
+          >
+            <v-avatar size="32px" tile width="32" min-width="32">
+              <v-img
+                v-if="$auth.user.picture"
+                :src="$auth.user.picture"
+                width="32"
+                height="32"
+                min-width="32"
+                alt="avatar"
+              />
+            </v-avatar>
+          </v-btn>
+          <v-btn
+            v-else
+            aria-label="login"
+            outlined
+            min-width="32"
+            @click="$auth.loginWith('custom')"
+          >
+            <v-icon>$account_circle</v-icon>
+            <span>&nbsp;</span>
+            <span>{{ $t('layout.login') }}</span>
+          </v-btn>
+        </template>
 
-      <v-card v-if="$auth.loggedIn">
-        <v-list>
-          <nuxt-link to="/profile">
-            <v-list-item avatar tile>
-              <v-list-item-avatar>
-                <v-img :src="$auth.user.picture" alt="avatar" class="rounded" />
-              </v-list-item-avatar>
+        <v-card v-if="$auth.loggedIn">
+          <v-list>
+            <nuxt-link to="/profile">
+              <v-list-item tile>
+                <v-list-item-avatar>
+                  <v-img
+                    :src="$auth.user.picture"
+                    alt="avatar"
+                    class="rounded"
+                    height="40"
+                    width="40"
+                  />
+                </v-list-item-avatar>
 
-              <v-list-item-content>
-                <v-list-item-title
-                  v-if="$auth.user.name !== $auth.user.email"
-                  >{{ $auth.user.name }}</v-list-item-title
-                >
-                <v-list-item-title>{{ $auth.user.email }}</v-list-item-title>
-              </v-list-item-content>
+                <v-list-item-content>
+                  <v-list-item-title
+                    v-if="$auth.user.name !== $auth.user.email"
+                    >{{ $auth.user.name }}</v-list-item-title
+                  >
+                  <v-list-item-title>{{ $auth.user.email }}</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </nuxt-link>
+          </v-list>
+
+          <v-divider />
+
+          <v-list>
+            <v-list-item @click="$auth.logout()">
+              <v-btn icon>
+                <v-icon>$run</v-icon>
+              </v-btn>
+              <v-list-item-title>{{ $t('layout.logout') }}</v-list-item-title>
             </v-list-item>
-          </nuxt-link>
-        </v-list>
-
-        <v-divider />
-
-        <v-list>
-          <v-list-item @click="$auth.logout()">
-            <v-btn icon>
-              <v-icon>$run</v-icon>
-            </v-btn>
-            <v-list-item-title>{{ $t('layout.logout') }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-card>
-    </v-menu>
-  </client-only>
+          </v-list>
+        </v-card>
+      </v-menu>
+      <v-btn slot="placeholder" aria-label="login" outlined min-width="32">
+        <v-icon>$account_circle</v-icon>
+        <span>&nbsp;</span>
+        <span>{{ $t('layout.login') }}</span>
+      </v-btn>
+    </client-only>
+  </div>
 </template>
 
 <script>
