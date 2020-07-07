37b0ff1856b92e37830ec7e170e9a2a8a6bfdb7e
Damien Robinson <damien.robinson@pocketpasta.com>
2020-07-07 21:55:13 +1000

    refactor(login): remove auth component

--- nuxt.config.js
+++ nuxt.config.js
@@ -72,7 +72,7 @@ module.exports = {
 
   workbox: {
     offlineAnalytics: true,
-    importScripts: ['/firebase-auth-sw.js'],
+    // importScripts: ['/firebase-auth-sw.js'],
     // by default the workbox module will not install the service worker in dev environment to avoid conflicts with HMR
     // only set this true for testing and remember to always clear your browser cache in development
     dev: false,
