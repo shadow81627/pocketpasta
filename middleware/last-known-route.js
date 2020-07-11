a05aa6b66c55721d457e06ab80d516b44ce1a6e5
Damien Robinson <damien.robinson@pocketpasta.com>
2020-07-11 17:38:28 +1000

    feat(routing): redirect to last known page
    Start off new session where the previous session left off.

---
+++ middleware/last-known-route.js
@@ -0,0 +1,20 @@
+export default function ({ route, $warehouse, app, redirect }) {
+  if (process.client) {
+    const LS_ROUTE_KEY = 'last_known_route';
+    const lastRoute = $warehouse.get(LS_ROUTE_KEY);
+    const shouldRedirect = Boolean(
+      route.path === app.localePath('index') &&
+        lastRoute &&
+        lastRoute !== route.path,
+    );
+    // save current route
+    $warehouse.set(LS_ROUTE_KEY, route.path);
+    if (shouldRedirect) {
+      window.onNuxtReady(() => {
+        window.$nuxt.$router.push(lastRoute);
+      });
+      // app.router.push(lastRoute);
+      // return redirect(lastRoute);
+    }
+  }
+}
