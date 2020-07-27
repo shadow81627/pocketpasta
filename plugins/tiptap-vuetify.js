c1fb00efa684f4a0ecf9073e087ecc0cffaf9243
Damien Robinson <damien.robinson@pocketpasta.com>
2020-07-27 22:49:14 +1000

    refactor(tiptap): webpack lazy chunk

--- plugins/tiptap-vuetify.js
+++ plugins/tiptap-vuetify.js
@@ -1,8 +1,12 @@
 import Vue from 'vue';
-import { TiptapVuetifyPlugin } from 'tiptap-vuetify';
+// import { TiptapVuetifyPlugin } from 'tiptap-vuetify';
 import 'tiptap-vuetify/dist/main.css';
 
-export default ({ app }) => {
+export default async ({ app }) => {
+  const TiptapVuetifyPlugin = (
+    await import(/* webpackChunkName: "tiptap" */ 'tiptap-vuetify')
+  ).TiptapVuetifyPlugin;
+
   Vue.use(TiptapVuetifyPlugin, {
     vuetify: app.vuetify,
     iconsGroup: 'mdiSvg',
