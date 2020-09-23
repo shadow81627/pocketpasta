fad78e6a99a07d3b95f55bad5a14e29a878b0bc3
Damien Robinson <damien.robinson@pocketpasta.com>
2020-09-23 14:32:54 +1000

    chore(lighthouse): non-composited-animations warn

--- components/Social/Share.vue
+++ components/Social/Share.vue
@@ -1,37 +1,7 @@
 <template>
-  <div
-    class="a2a_kit a2a_kit_size_32 a2a_default_style d-print-none"
-    style="min-height: 32px"
-  >
-    <a
-      class="a2a_dd"
-      href="https://www.addtoany.com/share"
-      aria-label="share"
-    />
-    <a class="a2a_button_facebook d-none d-md-block" />
-    <a class="a2a_button_twitter d-none d-md-block" />
-    <a class="a2a_button_email d-none d-md-block" />
-    <a class="a2a_button_reddit d-none d-md-block" />
-    <a class="a2a_button_pinterest d-none d-md-block" />
-    <a class="a2a_button_linkedin d-none d-md-block" />
-    <a class="a2a_button_copy_link d-none d-md-block" />
-    <a class="a2a_button_print d-none d-md-block" />
-  </div>
+  <div />
 </template>
 
 <script>
-export default {
-  head() {
-    return {
-      script: [
-        {
-          once: true,
-          skip: process.server,
-          async: '',
-          src: 'https://static.addtoany.com/menu/page.js',
-        },
-      ],
-    };
-  },
-};
+export default {};
 </script>
