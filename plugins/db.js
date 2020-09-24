8d4f06588a5377ba6cd0d1cb85b339bb3d4ed888
Damien Robinson <damien.robinson@pocketpasta.com>
2020-09-24 15:44:08 +1000

    perf(db): lazy load rxdb

--- plugins/db.js
+++ plugins/db.js
@@ -1,8 +1,17 @@
-import { createRxDatabase, addRxPlugin } from 'rxdb';
 import taskSchema from '@/db/schema/task.json';
-import indexeddb from 'pouchdb-adapter-indexeddb';
 
 export default async (context, inject) => {
+  const { createRxDatabase, addRxPlugin } = await import(
+    /* webpackChunkName: "db" */ 'rxdb'
+  );
+  const indexeddb = await import(
+    /* webpackChunkName: "db" */ 'pouchdb-adapter-indexeddb'
+  );
+
+  // const taskSchema = await import(
+  //   /* webpackChunkName: "db" */ '@/db/schema/task.json'
+  // );
+
   addRxPlugin(indexeddb);
 
   const db = await createRxDatabase({
