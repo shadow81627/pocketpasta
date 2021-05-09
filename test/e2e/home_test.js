4343e2352ecb91c86e4d17f84d9ca5d28761cad7
Damien Robinson <damien.robinson@daim.dev>
2021-05-10 09:23:10 +1000

    test(home): wait 2 seconds before screenshot

--- test/e2e/home_test.js
+++ test/e2e/home_test.js
@@ -10,6 +10,7 @@ Scenario('Heading 1', async ({ I }) => {
 
   I.click('#app-bar-nav-icon');
   I.see('Recipes', '#navigation-drawer');
+  I.wait(2); // wait 2 secs
 
   I.saveScreenshot(`${name}.png`);
   await I.seeVisualDiff(`${name}.png`);
