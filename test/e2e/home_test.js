5daaf40faa0b5172632a571d6e135aa6519983bc
Damien Robinson <damien.robinson@daim.dev>
2021-05-10 15:43:54 +1000

    test: add 5 second wait for shopping list

--- test/e2e/home_test.js
+++ test/e2e/home_test.js
@@ -10,7 +10,7 @@ Scenario('Heading 1', async ({ I }) => {
 
   I.click('#app-bar-nav-icon');
   I.see('Recipes', '#navigation-drawer');
-  I.wait(2); // wait 2 secs
+  I.wait(5);
 
   I.saveScreenshot(`${name}.png`);
   await I.seeVisualDiff(`${name}.png`);
--- test/e2e/shoppinglist_test.js
+++ test/e2e/shoppinglist_test.js
@@ -7,6 +7,7 @@ Feature(name);
 Scenario('Heading 1', async ({ I }) => {
   I.amOnPage(`/${name}`);
   I.see('Shopping', 'h1');
+  I.wait(5);
 
   I.saveScreenshot(`${name}.png`);
   await I.seeVisualDiff(`${name}.png`);
