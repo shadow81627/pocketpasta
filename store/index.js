36706fa97db033628009c4a60dc9195a2593d806
Damien Robinson <damien.robinson@daim.dev>
2021-05-20 18:33:12 +1000

    refactor: remove unused firebase config

--- store/index.js
+++ store/index.js
@@ -3,7 +3,6 @@ import products from '@/assets/link-data/products';
 import themes from '@/assets/themes';
 
 export const state = () => ({
-  user: null,
   themes,
   products,
   damien,
@@ -14,20 +13,4 @@ export const getters = {
     state.products.find((product) => product.id === id),
   getThemeById: (state) => (value) =>
     state.themes.find((theme) => theme.value === value),
-  getUser: (state) => {
-    return state.user;
-  },
-};
-
-export const mutations = {
-  setUser(currentState, user) {
-    // stupid hack to clone the object since something is modifying it
-    return (currentState.user = JSON.parse(JSON.stringify(user)));
-  },
-};
-
-export const actions = {
-  setUser: (context, user) => {
-    return context.commit('setUser', user);
-  },
 };
