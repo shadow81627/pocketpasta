7fd87c26fcc251bd495af9f1b7f0c8bceb4fc336
Damien Robinson <damien.robinson@pocketpasta.com>
2021-05-20 19:01:31 +1000

    fix(theme): timeout for mounted set theme (#2006)
    Co-authored-by: Damien Robinson <damien.robinson@daim.dev>

--- plugins/theme.js
+++ plugins/theme.js
@@ -1,5 +1,7 @@
 export default ({ $vuetify, $colorMode, store }) => {
   if (process.browser) {
-    $vuetify.theme.dark = store.getters.getThemeById($colorMode.value).dark;
+    setTimeout(() => {
+      $vuetify.theme.dark = store.getters.getThemeById($colorMode.value).dark;
+    }, 0);
   }
 };
