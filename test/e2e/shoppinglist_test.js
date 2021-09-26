709abc83ec72503afa795c4377d89c4750984bfe
Damien Robinson <damien.robinson@pocketpasta.com>
2021-09-26 21:11:40 +1000

    test(shoppinglist): add item (#2157)

--- test/e2e/shoppinglist_test.js
+++ test/e2e/shoppinglist_test.js
@@ -7,6 +7,18 @@ Feature(slug);
 Scenario('Heading 1', async ({ I }) => {
   I.amOnPage(`/${slug}`);
   I.see('Shopping', 'h1');
+  I.click('Create a new Shopping list item');
+  I.fillField('shopping-name', 'bread');
+  I.fillField('shopping-category', 'lunch');
+  I.click('Save');
+  I.see('lunch');
+  I.see('bread');
+  I.click('edit');
+  I.fillField('shopping-name', 'egg');
+  I.click('Save');
+  I.see('egg');
+  I.click('delete');
+  I.click('Yes');
   I.wait(5);
 
   I.saveScreenshot(`${slug}.png`);
