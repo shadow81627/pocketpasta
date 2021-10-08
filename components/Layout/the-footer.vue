e6fc945b52f22327a1d124363849cb76dce9368a
Damien Robinson <damien.robinson@pocketpasta.com>
2021-10-08 20:46:11 +1000

    refactor: remove releases page (#2168)
    * refactor: remove releases page
    
    * chore(footer): open releases in new tab
    
    Co-authored-by: Damien Robinson <damien.robinson@daim.dev>

--- components/Layout/the-footer.vue
+++ components/Layout/the-footer.vue
@@ -8,10 +8,11 @@
               <span>{{ $config.VERSION }}</span>
               <span v-if="$config.COMMIT">{{ shortHash($config.COMMIT) }}</span>
               <span v-if="$config.VERSION || $config.COMMIT">|</span>
-              <nuxt-link :to="localePath({ name: 'releases' })"
-                ><span>
-                  {{ shortHash($config.COMMIT) || 'Changelog' }}</span
-                ></nuxt-link
+              <a
+                href="https://github.com/shadow81627/pocketpasta/releases"
+                target="_blank"
+                rel="noopener"
+                ><span> {{ shortHash($config.COMMIT) || 'Changelog' }}</span></a
               >
             </v-card-text>
           </v-card>
