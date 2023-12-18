d5a9b2af459232c94baad78f77b61bad5a32e110
Damien Robinson <damien.robinson@pocketpasta.com>
2023-12-18 12:15:33 +1000

    fix(deps): add nuxt image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,37 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
+import { version } from "./package";
+
+const env = {
+  // HOST,
+  // PORT,
+  // BASE_URL,
+  // API_URL,
+  VERSION: version,
+  COMMIT:
+    process.env.npm_package_gitHead || process.env.VERCEL_GITHUB_COMMIT_SHA,
+  DATE_GENERATED: new Date().toISOString(),
+
+  CLOUDINARY_NAME: "pocketpasta",
+  IMAGE_PROVIDER: "cloudinary",
+  SENTRY_DSN:
+    process.env.SENTRY_DSN ||
+    "https://46d059ea02a240dfbb89bfaa4926f463@sentry.io/1411042",
+
+  DB_REMOTE: process.env.DB_REMOTE,
+  DB_USERNAME: process.env.DB_USERNAME,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+};
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   build: {
     transpile: ["vuetify"],
   },
-  modules: ["nuxt-icon", "@vite-pwa/nuxt"],
+
+  modules: ["nuxt-icon", "@vite-pwa/nuxt", "@nuxt/image"],
+
   pwa: {
     icons: [
       {
@@ -32,6 +58,67 @@ export default defineNuxtConfig({
       },
     ],
   },
+
+  image: {
+    provider: env.IMAGE_PROVIDER,
+    providers: {
+      cloudinary_upload: {
+        provider: "cloudinary",
+        options: {
+          baseURL: `https://res.cloudinary.com/${env.CLOUDINARY_NAME}/image/upload/`,
+        },
+      },
+    },
+    cloudinary: {
+      baseURL: `https://res.cloudinary.com/${env.CLOUDINARY_NAME}/image/fetch/`,
+      options: {
+        quality: "auto",
+        format: "auto",
+        dpr: "auto",
+      },
+    },
+    screens: {
+      placeholder: 10,
+      xs: 320,
+      sm: 600,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    },
+    domains: ["res.cloudinary.com"],
+    presets: {
+      hero: {
+        sizes: "xs:100vw sm:100vw md:100vw lg:100vw xl:100vw",
+        modifiers: {
+          quality: "auto",
+          format: "auto",
+          dpr: "auto",
+          // ar: '16:9',
+          // c: 'fill',
+        },
+      },
+      og: {
+        sizes: "xs:100vw sm:100vw md:100vw lg:100vw xl:100vw",
+        modifiers: {
+          quality: "auto",
+          format: "auto",
+          dpr: "auto",
+          // height: 630,
+          width: 1280,
+          c: "fill",
+        },
+      },
+      placeholder: {
+        modifiers: {
+          quality: "auto",
+          format: "auto",
+          dpr: "auto",
+          width: 10,
+        },
+      },
+    },
+  },
+
   vite: {
     vue: {
       template: {
